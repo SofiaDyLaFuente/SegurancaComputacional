22a8cd35dcf045c9f7a9cbe4c84a0230395bffb1
Sofia Dy La Fuente Monteiro <150842264+SofiaDyLaFuente@users.noreply.github.com>
2025-04-21 18:20:25 -0300

    Add files via upload

---
+++ LE1/ShiftCipher.py
@@ -0,0 +1,135 @@
+from unidecode import unidecode
+from collections import Counter
+
+# Função de ataque por distribuição de frequência
+def frequencia(cipherText):
+
+    #Percentagens de frequência dos caracteres em Português.
+    frequencias = {
+        13.9, 1.0, 4.4, 5.4, 12.2, 1.0,
+        1.2, 0.8, 6.9, 0.4, 0.1, 2.8, 4.2, 
+        5.3, 10.8, 2.9, 0.9, 6.9, 7.9, 
+        4.9, 4.0, 1.3, 0.0, 0.3, 0.0, 0.4,
+    }
+
+
+# Função de ataque de força bruta
+def forcaBruta(cipherText):
+
+    for chave in range(1, 26): 
+        listaDecriptografado = []
+        
+        for caracter in cipherText:
+            charAscii = ord(caracter.lower())
+            
+            if charAscii < 97 or charAscii > 122:
+                listaDecriptografado.append(caracter)
+                continue
+            
+            charAscii -= chave
+                
+            # Se o valor ASCII for menor que 97 soma 26 para fazer a volta
+            if charAscii < 97:
+                charAscii += 26
+                
+            listaDecriptografado.append(chr(charAscii))
+        
+        print(f"Possível texto cifrado com chave {chave}:\n{''.join(listaDecriptografado)}")
+
+
+# Função de encriptação
+def encriptacao(chave):
+    listaCriptografado = []
+    plainText = unidecode(input("Digite um texto para ser cifrado: "))
+
+    # Codigo ASCII vai de 97 a 122 (minusculo)
+
+    for caractere in plainText:
+        charAscii = ord(caractere.lower())
+        chave1 = chave
+
+        while chave1 != 0:
+
+            if charAscii < 97 or charAscii > 122:
+                pass
+
+            elif charAscii == 122:
+                charAscii = 97
+
+            else: 
+                charAscii +=1
+
+            chave1 -= 1      
+
+        listaCriptografado.append(chr(charAscii))
+
+    cipherText = ''.join(listaCriptografado)
+    print(f"Seu texto cifrado é \n{cipherText} \n")
+
+    return cipherText
+
+# Função de decriptação
+def Decriptacao(chave, cipherText):
+    # Decriptar é o mesmo que encriptar, só que a chave é negativa
+    
+    listaDecriptografado = []
+
+    # Codigo ASCII vai de 97 a 122 (minusculo)
+
+    for caractere in cipherText:
+        charAscii = ord(caractere.lower())
+        chave1 = chave
+
+        while chave1 != 0:
+            if charAscii < 97 or charAscii > 122:
+                pass
+
+            elif charAscii == 97:
+                charAscii = 122
+
+            else: 
+                charAscii -=1
+
+            chave1 -= 1          
+
+        listaDecriptografado.append(chr(charAscii))
+
+    print(f"Seu texto em claro é \n{''.join(listaDecriptografado)} \n")
+    
+
+# Função principal
+def main():
+    opcao = int(input(f'Escolha uma opção: \n 1 - Encriptar \n 2 - Decriptar \n 3 - Força Bruta \n 4 - Distribuição de Frequência \n 5 - Sair \n'))
+
+    if opcao < 1 or opcao > 5:
+        print("Opção inválida!")
+        main()
+    
+    # Se for as opções de encriptar ou decriptar a chave é necessária
+    if opcao == 1 or opcao == 2:
+        chave = int(input("Digite a chave k de 0 a 25: ")) % 26    # Caso o usuário digite um número maior que 25, o módulo 26 garante que a chave fique entre 0 e 25.
+
+    if opcao == 1:
+        cipherText = encriptacao(chave)
+        main()
+
+    elif opcao == 2:
+        cipherText = unidecode(input("Digite o texto cifrado: "))
+        Decriptacao(chave, cipherText)
+        main()
+
+    elif opcao == 3:
+        cipherText = unidecode(input("Digite o texto cifrado: "))
+        forcaBruta(cipherText)
+        main()
+    
+    elif opcao == 4:
+        cipherText = unidecode(input("Digite o texto cifrado: "))
+        frequencia(cipherText)
+        main()
+    
+    elif opcao == 5:
+        print("Fim")
+
+main()
+
